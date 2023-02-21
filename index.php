fa994bf47fd665d7226a2db1f385dacf3cb69000
Gabr19 <sannagabriele19@gmail.com>
2023-02-21 17:43:34 +0100

    .

--- index.php
+++ index.php
@@ -2,16 +2,46 @@
 
     $pwdVar = $_GET['pwd'];
 
-    $password [] = '';
 
-     for ($i=0; $i<$pwdVar; $i++){
-         $numero = rand(0,10);
+    //  for ($i=0; $i<$pwdVar; $i++){
+    //      $numero = rand(0,10);
 
-         $password[] = $numero;
-     };
+    //      $password[] = $numero;
+    //  };
 
+    // $arr=
+    // ['a']; 
+    // ['b']; 
+    // ['c'];
 
+      
+    // $lettereMin["minuscole"] = 'abcdefghijklmnopqrstuvwxyz';
+    // $lettereMaiusc["maiuscole"] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // $numeri["numeri"] = '1234567890';
+    // $caratteriSpec["caratteri_speciali"] = '!?~@#-_+<>[]{}';
 
+    // $totaleCaratteri = array_merge($lettereMin, $lettereMaiusc, $numeri, $caratteriSpec);
+
+    // for ($i=0; $i<$pwdVar; $i++){
+    //     foreach ($totaleCaratteri as $key => $value){
+    //         $carattere = $totaleCaratteri[rand(0, strlen($value) )] ;
+
+    //         $password[] = $carattere;
+    //     }
+    // };
+
+    function generaStringaRandom($pwdVar) {
+
+        $caratteri = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?~@#-_+<>[]{}';
+        $stringaRandom = '';
+        for ($i = 0; $i < $pwdVar; $i++) {
+            $stringaRandom = $caratteri[rand(0, strlen($caratteri) - 1)];
+        }
+        return $stringaRandom;
+        
+    }
+
+    $arrPasswordCompleta [] += $stringaRandom;
 
 
 
@@ -34,14 +64,21 @@
         <button>Invia</button>
     </form>
 
-    <h1><?php echo "la lunghezza della password è: $pwdVar"?></h1>
+    <h1><?php echo "La lunghezza della password è: $pwdVar"?></h1>
+    <h4>La tua password è: <?php print_r(generaStringaRandom($_GET['pwd'])) ?></h4>
 
-    <?php
-    //    var_dump($password[])
+
+     <!-- <?php
     foreach ($password as $key => $value){
-        print_r($value) ;
+        print_r($value);
+        
     }
-    ?>
+    ?>  -->
+
+    <!-- <?php foreach ($totaleCaratteri as $key => $value){
+        print_r($value);
+        
+    } ?> -->
     
 
 
